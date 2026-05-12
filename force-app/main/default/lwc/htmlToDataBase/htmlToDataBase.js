9aa49a029a4edfbf1e831174c955ae5a2d071055
Himadri-sfdc <paul.himadri.sfdc@gmail.com>
2026-05-13 03:03:37 +0530

    We are creating Record From Html and Saving it To DataBase

--- force-app/main/default/lwc/htmlToDataBase/htmlToDataBase.js
+++ force-app/main/default/lwc/htmlToDataBase/htmlToDataBase.js
@@ -1,3 +1,12 @@
 import { LightningElement } from 'lwc';
+import createCon from '@salesforce/apex/ContactController.createCon';
+export default class HtmlToDataBase extends LightningElement {
 
-export default class HtmlToDataBase extends LightningElement {}
+    name = '';
+    handleName(event){
+        this.name = event.target.value;
+    }
+    saveContact(){
+        createCon({lname : this.name});
+    }
+}
